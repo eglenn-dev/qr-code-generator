dc53dd5118b983101146655ae6cff05191f509ce
Ethan Glenn <ethan+git@eglenn.dev>
2023-12-30 13:36:35 -0700

    Added comments to app.py

--- app.py
+++ app.py
@@ -6,27 +6,33 @@ qr_code_filepath = ''
 
 @app.route('/')
 def home():
+    # Serving the index when the root domain is accessed
     return render_template('index.html')
 
 @app.route('/qr')
 def qr():
+    # Serve the form for the qr code customization
     return render_template('qr.html')
 
 @app.route('/qr-img', methods=['POST'])
 def qr_img():
+    # Trying to generate the qr code with the inputted information
     try:
+        # Run the function that will generate the qr code and return the name with the filepath.
         qr_code_filepath = make_qr_code(url=request.form['urlInput'], scale=request.form['scale'], background=request.form['backgroundColor'], code_color=request.form['codeColor'], border_color=request.form['borderColor'])
+        # Render the page with the QR code image path 
         return render_template('qr-img.html', qr_code_img_path=qr_code_filepath)
     except:
+        # If there is an error generating the QR code, return the error page
         return render_template('error.html')
 
 @app.route('/<short_url>')
 def page_not_found(short_url):
+    # Return the 404 page for all other domain sub-page requests
     return render_template('404.html', short_url=short_url)
 
 if __name__ == '__main__':
-    # Runs the app on default port and on broadcasts on all channels.
-    # This is done for the live deployment versions of the app. 
+    # Runs the app on default port and on broadcasts on all channels. This is done for the live deployment versions of the app. 
     app.run(port=5000, host='0.0.0.0')
 
     # Runs the app on the local server 127.0.0.1:5001
